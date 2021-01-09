8362792699c6029168a711915cf411fd2736452a
Ifanj <ifan.johnston@gmail.com>
2021-01-09 16:30:39 +0100

    Setting up cron jobs
    Set up refresh of notebooks every day (initlally every 30 minutes to test). `update-nb.yaml` will run the `run_notebooks.sh` according to the cron job schedule, and will run the notebooks and convert the output to a post then publish the new site

---
+++ _action_files/run_notebooks.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -e
+cd $(dirname "$0")/..
+cd _notebooks/
+
+ERRORS=""
+
+for file in *.ipynb
+do
+    if [ "${file}" = "2020-11-29-sentiment.ipynb" ]; then
+        echo "Skipping ${file}"
+    elif papermill --kernel python3 "${file}" "${file}"; then
+        echo "Successfully refreshed ${file}\n\n\n\n"
+    else
+        echo "Error Refreshing ${file}"
+        ERRORS="${ERRORS}, ${file}"
+    fi
+done
+
+# Emit Errors if Exists so Downstream Task can open an issue
+if [ -z "$ERRORS" ]
+then
+    echo "::set-output name=error_bool::false"
+else
+    echo "These files failed to update properly: ${ERRORS}"
+    echo "::set-output name=error_bool::true"
+    echo "::set-output name=error_str::${ERRORS}"
+fi
