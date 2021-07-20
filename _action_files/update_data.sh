dcccc314d783719e0a7720dcbaf4f6a0a82a3a95
Ifanj <ifan.johnston@gmail.com>
2021-07-20 15:22:04 +0200

    Adding ciphers option to the curl request to avoid dh key

--- _action_files/update_data.sh
+++ _action_files/update_data.sh
@@ -3,7 +3,7 @@ set -e
 cd $(dirname "$0")/..
 cd src/
 
-curl "https://covid19-dashboard.ages.at/data/CovidFaelle_Timeline.csv" -o ../data_sets/CovidFaelle_Timeline.csv
-curl "https://covid19-dashboard.ages.at/data/CovidFallzahlen.csv" -o ../data_sets/CovidFallzahlen.csv
+curl "https://covid19-dashboard.ages.at/data/CovidFaelle_Timeline.csv" -o ../data_sets/CovidFaelle_Timeline.csv --ciphers 'DEFAULT:!DH'
+curl "https://covid19-dashboard.ages.at/data/CovidFallzahlen.csv" -o ../data_sets/CovidFallzahlen.csv --ciphers 'DEFAULT:!DH'
 
 python3 update_datasets.py && git add ../data_sets
